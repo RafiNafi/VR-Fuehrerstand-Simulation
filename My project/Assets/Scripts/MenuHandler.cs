7b601db860400fdbd695c6f75071d80ee9a22a67
Rafi <g-rafael.konecsni@outlook.de>
2025-01-10 12:41:20 +0100

    added basic menu

---
+++ My project/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public Toggle freeMode;
+    public Toggle processMode;
+    public Toggle exameMode;
+    public TMP_Dropdown selectProcess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startMode()
+    {
+
+    }
+}
