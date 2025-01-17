734c914344ca059cd49bf6cf3dce02d235ba578c
Rafi <g-rafael.konecsni@outlook.de>
2025-01-17 13:49:03 +0100

    added save and loading scenario json

--- My project/Assets/Scripts/MenuHandler.cs
+++ My project/Assets/Scripts/MenuHandler.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Playables;
+using System.Xml;
+using UnityEngine.InputSystem;
 
 public class MenuHandler : MonoBehaviour
 {
@@ -11,10 +15,28 @@ public class MenuHandler : MonoBehaviour
     public Toggle exameMode;
     public TMP_Dropdown selectProcess;
 
+    private string persistentFilePath;
+    private string sourcePath;
+
+    ScenarioCollection sc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        persistentFilePath = Application.persistentDataPath + "/scenarios.json";
+        sourcePath = Application.streamingAssetsPath + "/scenarios.json";
+
+        if (!File.Exists(persistentFilePath))
+        {
+            copyFileToPersistentPath();
+        }
+
+        sc = loadScenario();
+
+        addDropdownOptions();
+
+        selectProcess.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -32,11 +54,27 @@ public class MenuHandler : MonoBehaviour
         else if (processMode.isOn)
         {
             toggleAllTooltips(false);
+            startScenario();
         }
         else if (exameMode.isOn)
         {
             toggleAllTooltips(false);
+            startScenario();
         }
+    }
+
+    public void startScenario()
+    {
+        string name = selectProcess.options[selectProcess.value].text;
+
+        foreach (Scenario scenario in sc.scenarios)
+        {
+            if (scenario.name == name)
+            {
+                //TODO
+            }
+        }
+        
     }
 
     public void toggleAllTooltips(bool activate)
@@ -49,6 +87,94 @@ public class MenuHandler : MonoBehaviour
             {
                 script.enabled = activate;
             }
+        }
+    }
+
+    public void checkTooltip()
+    {
+        if (freeMode.isOn)
+        {
+            selectProcess.gameObject.SetActive(false);
+        }
+        else if (processMode.isOn)
+        {
+            selectProcess.gameObject.SetActive(true);
+        }
+        else if (exameMode.isOn)
+        {
+            selectProcess.gameObject.SetActive(true);
+        }
+    }
+
+    [System.Serializable]
+    public class Scenario
+    {
+        public string name;
+        public List<string> objects;
+    }
+
+    [System.Serializable]
+    public class ScenarioCollection
+    {
+        public List<Scenario> scenarios;
+    }
+
+
+
+    public void saveScenario(ScenarioCollection data)
+    {
+        string jsonData = JsonUtility.ToJson(data, true);
+
+        File.WriteAllText(persistentFilePath, jsonData);
+    }
+
+    public ScenarioCollection loadScenario()
+    {
+        if (File.Exists(persistentFilePath))
+        {
+            string jsonData = File.ReadAllText(persistentFilePath);
+            ScenarioCollection data = JsonUtility.FromJson<ScenarioCollection>(jsonData);
+
+            return data;
+        }
+        else
+        {
+            Debug.LogWarning("File not found.");
+            return null;
+        }
+    }
+
+    public void copyFileToPersistentPath()
+    {
+        string sourcePath = Path.Combine(Application.streamingAssetsPath, "scenarios.json");
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            var www = new WWW(sourcePath);
+            while (!www.isDone) { }
+            File.WriteAllBytes(persistentFilePath, www.bytes);
+        }
+        else
+        {
+            File.Copy(sourcePath, persistentFilePath);
+        }
+
+    }
+
+    void addDropdownOptions()
+    {
+        if (sc != null)
+        {
+            List<string> options = new List<string>();
+
+            foreach (var scenario in sc.scenarios)
+            {
+                options.Add(scenario.name);
+            }
+
+            selectProcess.ClearOptions();
+
+            selectProcess.AddOptions(options);
         }
     }
 }
